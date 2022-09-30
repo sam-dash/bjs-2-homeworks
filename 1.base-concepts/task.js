b1ce3110c599c7b23409022a6fdc99974b1683ec
Daria <sam-dash@yandex.ru>
2022-09-30 22:12:29 +0300

    1homework

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,17 @@
+"use strict";
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+  let discriminant = Math.pow(b, 2) - (4 * a * c);
+  if (discriminant > 0) {
+    let firstSum = ((-1 * b) + Math.sqrt(discriminant)) / (2 * a); 
+    arr.push(firstSum);
+    let secondSum = ((-1 * b) - Math.sqrt(discriminant)) / (2 * a); 
+    arr.push(secondSum);
+  }
+  else if (discriminant === 0) {
+    let thirdSum = (-1 * b) / (2 * a);
+    arr.push(thirdSum);
+  }
+  return arr; 
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
-
-  // код для задачи №2 писать здесь
-
-  return totalAmount;
-}
