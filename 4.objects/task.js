40ee55882cf0a3993a410b16a943de0fc71200da
Daria <sam-dash@yandex.ru>
2022-10-13 22:29:31 +0300

    4homework

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,37 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function(mark) {
+  this.marksLazyInitialization();
+  this.marks.push(mark);
+}
+
+Student.prototype.addMarks = function(...marks) {
+  this.marksLazyInitialization();
+  this.marks = this.marks.concat(...marks);
+}
+
+Student.prototype.getAverage = function() {
+  let sum = this.marks.reduce((accumulator, currentValue) => accumulator + currentValue);
+  let average = sum / this.marks.length;
+  return average;
+}
+
+Student.prototype.exclude= function(reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
+
+Student.prototype.marksLazyInitialization = function() {
+  if(this.marks === undefined) {
+    this.marks = [];
+  }
+}
