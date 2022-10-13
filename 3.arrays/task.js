1e40575df06714c1a5f8c0c34fe88c2c67f03c98
Daria <sam-dash@yandex.ru>
2022-10-13 22:12:43 +0300

    3homework

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,21 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+  let result = false;
+ 
+  if(!Array.isArray(arr1) || !Array.isArray(arr2)) {
+    return result;
+  }
+  if(arr1.length !== arr2.length) {
+    return result;
+  }
+  
+  result = arr1.every((item, index) => item === arr2[index]);
+  return result; 
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+  let resultArr = arr.filter((item) => item > 0
+  ).filter((item) => item % 3 === 0
+  ).map((item) => item * 10
+  );
+  return resultArr;
 }
